62f63b6c93fb1befd3f3be5358f50cb052f99030
Taiye <taiyeajimati@gmail.com>
2022-10-01 23:58:36 +0400

    Last home commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import heroimg from '../../img/image-mockups.png'
 
 export default function Hero() {
     return (
-        <div>
-            <div className="desc"></div>
-            <div className="hero-img"></div>
+        <div
+            className='flex flex-col-reverse 
+        md:flex-row
+        '>
+            <div className="container desc flex-1 border-2 text-left text-center">
+                <h1 className='mb-[20px]'>Next generation digital banking</h1>
+                <p className='body'>Take your financial life online. Your Easybank account will be a one-stop-shop
+                    for spending, saving, budgeting, investing, and much more.</p>
+            </div>
+            <div className="hero-img flex container flex-col items-center flex-1 border-2 overflow-hidden">
+                <img className='mt-[-140px] ' src={heroimg} alt="" /></div>
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
     colors: {
       'DarkBlue': '#2d314d',
       'LimeGreen': '#31d35c',
-      'BrightCyan': '#2bb7da'
+      'BrightCyan': '#2bb7da',
+      'GrayishBlue': '#9698a6',
+      'body-grey': '#f3f4f6',
+      'VeryLightGray': '#f3f4f6',
+      'White': '#ffffff',
     },
     container: {
       padding: {
